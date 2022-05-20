166442cc2ab4f7facae43c682c44f26ce6b343fd
Ivelin Borisov <90949939+borisov5@users.noreply.github.com>
2022-05-20 21:25:45 +0300

    Delete week 10 directory

--- week 10/Exercise/02. Find Variable Names in Sentences.py
+++
@@ -1,6 +0,0 @@
-import re
-
-some_string = input()
-pattern = r'\b_([A-Za-z0-9]+)\b'
-matches = re.findall(pattern, some_string)
-print(','.join(matches))
--- week 10/Exercise/03. Find Occurrences of Word in Sentence.py
+++
@@ -1,7 +0,0 @@
-import re
-
-some_string = input()
-searched_word = input()
-pattern = fr'\b{searched_word}\b'
-matches = re.findall(pattern, some_string, flags=re.I)
-print(len(matches))
